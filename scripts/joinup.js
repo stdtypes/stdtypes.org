d36ae6c2fd2bc721697ad9695a8cb9949a37d12f
jforster <johann@forster.family>
2018-05-15 13:02:30 +0200

    removed legacy files

--- scripts/joinup.js
+++
@@ -1,9 +0,0 @@
-
-const
-  fs = require("fs"),
-  joinup = require("../lib/joinup");
-
-var input = fs.readFileSync(process.argv[2], {encoding: "utf8"});
-var project = JSON.parse(input);
-
-joinup(project);
